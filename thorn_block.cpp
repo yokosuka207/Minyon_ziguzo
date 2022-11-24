48f612b222ed2e87f7fe25846369652574608b7d
kimuzero <tochitakafusegi@gmail.com>
2022-11-24 22:05:26 +0900

    4

--- thorn_block.cpp
+++ thorn_block.cpp
@@ -1,21 +1,62 @@
+/*==============================================================================
+
+   トゲブロック [thorn_block.cpp]
+														 Author		:座間研佑
+														 Date		:2022/11/09
+														 最終更新	:2022/11/224
+==============================================================================*/
+
 #include "thorn_block.h"
+#include "main.h"
+#include "polygon.h"
 #include "texture.h"
 #include "sprite.h"
 #include "input.h"
+#include "player.h"
+#include "time.h"
+#include "scene.h"
 #include "puzzle.h"
 #include "broken.h"
 #include "collision.h"
 #include "player.h"
 #include "game.h"
 
-THORN_BLOCK g_ThornBlock[THORN_BLOCK_MAX];
+THORNBLOCK g_ThornBlock[THORN_BLOCK_MAX];
 static ID3D11ShaderResourceView	*g_textureThornBlock;	//画像一枚で一つの変数が必要
 static char *g_textureName_ThornBlock = (char*)"data\\texture\\thorn_block.png";	//テクスチャファイルパス
 
+//=============================================================================
+//マクロ定義
+//=============================================================================
+
+//=============================================================================
+//プロトタイプ宣言
+//=============================================================================
+
+//=============================================================================
+//グローバル変数
+//=============================================================================
+static THORNBLOCK g_ThornBlock[THORN_BLOCK_MAX];
+static ID3D11ShaderResourceView* g_textureThornBlock;	//画像一枚で一つの変数が必要
+
+static char* g_TextureNameThornBlock = (char*)"data\\texture\\thorn_block.png";
+
+THORNBLOCK InitData[] =
+{
+	{true,D3DXVECTOR2(THORN_BLOCK_W,THORN_BLOCK_H),D3DXVECTOR2(400,100),D3DXVECTOR2(0,2),0,0,D3DXCOLOR(1,0,0,1),1,8,16,8,60 * 0},
+};
+
+
+//=============================================================================
+//初期化処理
+//=============================================================================
 HRESULT InitThornBlock()
 {
+	srand(time(NULL));
+
 	for (int i = 0; i < THORN_BLOCK_MAX; i++)
 	{
+<<<<<<< HEAD
 		g_ThornBlock[i].texno = LoadTexture(g_textureName_ThornBlock);
 
 		g_ThornBlock[i].Position = D3DXVECTOR2(0.0f, 0.0f);
@@ -27,18 +68,36 @@ HRESULT InitThornBlock()
 
 	}
 
+=======
+		g_ThornBlock[i].Postion = D3DXVECTOR2(0.0f, 0.0f);
+		g_ThornBlock[i].Size = D3DXVECTOR2(THORN_BLOCK_W, THORN_BLOCK_H);
+		g_ThornBlock[i].texno = LoadTexture(g_TextureNameThornBlock);
+		g_ThornBlock[i].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);;
+		g_ThornBlock[i].UseFlag = false;
+	}
+>>>>>>> 90b99b71e27210bd6a8deaafb35e92c6ebed0d71
 	return S_OK;
 }
 
+//=============================================================================
+//終了処理
+//=============================================================================
 void UninitThornBlock()
 {
-	if (g_textureThornBlock)
+	for (int i = 0; i < THORN_BLOCK_MAX; i++)
 	{
+<<<<<<< HEAD
 		g_textureThornBlock->Release();
 		g_textureThornBlock = NULL;
+=======
+
+>>>>>>> 90b99b71e27210bd6a8deaafb35e92c6ebed0d71
 	}
 }
 
+//=============================================================================
+//更新処理
+//=============================================================================
 void UpdateThornBlock()
 {
 	PLAYER* pPlayer = GetPlayer();
@@ -49,8 +108,12 @@ void UpdateThornBlock()
 	}
 }
 
+//=============================================================================
+//描画処理
+//=============================================================================
 void DrawThornBlock()
 {
+<<<<<<< HEAD
 	for (int i = 0; i < THORN_BLOCK_MAX; i++){
 		if (g_ThornBlock[i].UseFlag){
 			SetWorldViewProjection2D();
@@ -62,11 +125,26 @@ void DrawThornBlock()
 			SpriteDrawColorRotation(g_ThornBlock[i].Position.x, g_ThornBlock[i].Position.y,
 				g_ThornBlock[i].Size.x, g_ThornBlock[i].Size.y, g_ThornBlock[i].Rotation, g_ThornBlock[i].Col,
 				0, 1.0f, 1.0f, 1);
+=======
+	for (int i = 0; i < THORN_BLOCK_MAX; i++)
+	{
+		if (g_ThornBlock[i].UseFlag)
+		{
+			SetWorldViewProjection2D();
+
+			//テクスチャの設定
+			GetDeviceContext()->PSSetShaderResources(0, 1, GetTexture(g_ThornBlock[i].texno));
+			//スプライトを表示
+			D3DXCOLOR col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+			SpriteDrawColorRotation(g_ThornBlock[i].Postion.x, g_ThornBlock[i].Postion.y, g_ThornBlock[i].Size.x, g_ThornBlock[i].Size.y,
+				g_ThornBlock[i].rot, g_ThornBlock[i].col, 0, 1.0f, 1.0f, 1);
+
+>>>>>>> 90b99b71e27210bd6a8deaafb35e92c6ebed0d71
 		}
 	}
-
 }
 
+<<<<<<< HEAD
 void SetThornBlock(D3DXVECTOR2 pos, D3DXVECTOR2 size, int PieceNo) {
 	for (int i = 0; i < THORN_BLOCK_MAX; i++) {
 		if (!g_ThornBlock[i].UseFlag) {
@@ -81,13 +159,17 @@ void SetThornBlock(D3DXVECTOR2 pos, D3DXVECTOR2 size, int PieceNo) {
 
 
 void SetThornBlock(D3DXVECTOR2 pos, D3DXVECTOR2 size, THORN_GRAND_TYPE type, THORN_BLOCK_TYPE btype, int pIndex)
+=======
+int SetThornBlock(D3DXVECTOR2 Pos, D3DXVECTOR2 s)
+>>>>>>> 90b99b71e27210bd6a8deaafb35e92c6ebed0d71
 {
-	PUZZLE* pPuzzle = GetPuzzle();
+	//PUZZLE* pPuzzle = GetPuzzle();
 
 	for (int i = 0; i < THORN_BLOCK_MAX; i++)
 	{
 		if (!g_ThornBlock[i].UseFlag)
 		{
+<<<<<<< HEAD
 			if (btype != THORN_TYPE_NUM)
 			{
 				switch (btype)
@@ -296,13 +378,30 @@ void SetThornBlock(D3DXVECTOR2 pos, D3DXVECTOR2 size, THORN_GRAND_TYPE type, THO
 
 			g_ThornBlock[i].UseFlag = true;
 			break;
+=======
+
+			g_ThornBlock[i].Postion = Pos;
+			g_ThornBlock[i].Size = s;
+			g_ThornBlock[i].UseFlag = true;
+			return i;
+>>>>>>> 90b99b71e27210bd6a8deaafb35e92c6ebed0d71
 
 		}
+
+
 	}
 
 }
 
-THORN_BLOCK* GetThornBlock()
+//=============================================================================
+//ゲット関数
+//=============================================================================
+THORNBLOCK* GetThornBlock()
 {
+<<<<<<< HEAD
 	return &g_ThornBlock[0];
 }
+=======
+	return g_ThornBlock;
+}
+>>>>>>> 90b99b71e27210bd6a8deaafb35e92c6ebed0d71
