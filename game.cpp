b5879f061dd525d240f876bd72ecad72c9015556
SaitoReito <reaizy44@gmail.com>
2023-01-25 16:52:03 +0900

    g

--- game.cpp
+++ game.cpp
@@ -180,7 +180,7 @@ void UpdateGame()
 	//ポーズ処理
 	if (Keyboard_IsKeyTrigger(KK_TAB) ||				// keyboard TAB
 		IsButtonTriggered(0, XINPUT_GAMEPAD_START) ||	// GamePad START
-		IsButtonTriggered(0, XINPUT_GAMEPAD_BACK)) {		// Gamepad BACK
+		IsButtonTriggered(0, XINPUT_GAMEPAD_BACK)) {	// Gamepad BACK
 		//ポーズフラグがoff
 		if (!(*pause)) {
 			(*pause) = true;
