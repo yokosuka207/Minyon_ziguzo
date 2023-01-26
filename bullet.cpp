4510cc622930112f5131e453daa28b362e4f9de2
shimboshoma <shimbo.shoma@gmail.com>
2023-01-26 10:44:42 +0900

    a

--- bullet.cpp
+++ bullet.cpp
@@ -106,7 +106,19 @@ void UpdateBullet()
 
 
 		}
-		if (g_Bullet[i].pos.y < SCREEN_LIMIT_UP - (g_Bullet[i].h / 2.0f))
+		if (g_Bullet[i].pos.y < -SCREEN_HEIGHT/2 + (g_Bullet[i].h / 2.0f))
+		{
+			g_Bullet[i].use = false;
+		}
+		if (g_Bullet[i].pos.y > SCREEN_HEIGHT / 2 - (g_Bullet[i].h / 2.0f))
+		{
+			g_Bullet[i].use = false;
+		}
+		if (g_Bullet[i].pos.x < -SCREEN_WIDTH/2 - (g_Bullet[i].w / 2.0f))
+		{
+			g_Bullet[i].use = false;
+		}
+		if (g_Bullet[i].pos.x > SCREEN_WIDTH / 2 - (g_Bullet[i].w / 2.0f))
 		{
 			g_Bullet[i].use = false;
 		}
