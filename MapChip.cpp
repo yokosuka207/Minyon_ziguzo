3e5e04d01aa4e412425da0cdbb107cb559c10954
ha-tiryu <frankey8122@gmail.com>
2023-01-27 15:16:44 +0900

    ステージ修正

--- MapChip.cpp
+++ MapChip.cpp
@@ -640,16 +640,16 @@ void SetField(D3DXVECTOR2 position, D3DXVECTOR2 DrawSize, int no, int Pin, int i
 	case static_cast<int>(MAPCHIP_TYPE::TYPE_SPWANPOINT_D)://37　ドッペルゲンガーの初期位置?
 		SetSpawnPointD(position, DrawSize, no);
 		break;
-	case static_cast<int>(MAPCHIP_TYPE::TYPE_PIACEEXPLAIN4):	//38 ③ヒントブロック(Piace)
+	case static_cast<int>(MAPCHIP_TYPE::TYPE_PIACEEXPLAIN4):	//38 ④ヒントブロック(Piace)
 		SetExplain(position, DrawSize, no, g_PieceMapChip[no].direction, 38);
 		break;
-	case static_cast<int>(MAPCHIP_TYPE::TYPE_PIACEEXPLAIN5):	//39 ③ヒントブロック(Piace)
+	case static_cast<int>(MAPCHIP_TYPE::TYPE_PIACEEXPLAIN5):	//39 ⑤ヒントブロック(Piace)
 		SetExplain(position, DrawSize, no, g_PieceMapChip[no].direction, 39);
 		break;
-	case static_cast<int>(MAPCHIP_TYPE::TYPE_PIACEEXPLAIN6):	//40 ③ヒントブロック(Piace)
+	case static_cast<int>(MAPCHIP_TYPE::TYPE_PIACEEXPLAIN6):	//40 ⑥ヒントブロック(Piace)
 		SetExplain(position, DrawSize, no, g_PieceMapChip[no].direction, 40);
 		break;
-	case static_cast<int>(MAPCHIP_TYPE::TYPE_PIACEEXPLAIN7):	//41 ③ヒントブロック(Piace)
+	case static_cast<int>(MAPCHIP_TYPE::TYPE_PIACEEXPLAIN7):	//41 ⑦ヒントブロック(Piace)
 		SetExplain(position, DrawSize, no, g_PieceMapChip[no].direction, 41);
 		break;
 	default:
