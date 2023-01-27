39171871c09425b4d44754aeb4f7fe0ad2763a55
稲葉陸斗 <hal.offret1986@gmail.com>
2023-01-27 11:27:54 +0900

    a

--- StageSelect.cpp
+++ StageSelect.cpp
@@ -543,7 +543,7 @@ void UpdateStageSelect() {
 			}
 			else
 			{
-				g_StageSelect[i].size = D3DXVECTOR2(300.0f, 140.0f);
+				g_StageSelect[i].size = D3DXVECTOR2(330.0f, 180.0f);
 
 				if (ply.Position.x - ply.size.x / 2 > g_StageSelect[i].pos.x - g_StageSelect[i].size.x / 2 &&
 					ply.Position.x + ply.size.x / 2 < g_StageSelect[i].pos.x + g_StageSelect[i].size.x / 2 &&
